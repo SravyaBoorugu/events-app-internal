f5e759fd2e1a7af9aa506e09a2b909aab25413d6
SravyaBoorugu <shravyaboorugu@gmail.com>
2021-09-16 07:10:52 +0000

    changed event name

--- test/test-server.js
+++ test/test-server.js
@@ -57,7 +57,7 @@ describe('GET /', function() {
     it('adds an event', function(done) {
       request(app)
       .post('/event')
-      .send( { title: 'a sample event', description: 'a really really cool test' })
+      .send( { title: 'new title name', description: 'new cool description' })
       .set('Accept', 'application/json')
       .expect(200)
       .end((err, res) => {
